98af2c19d05fb2abc919eb731a78430e8c6f51e2
vulcandth <vulcandth@gmail.com>
2022-10-13 23:48:14 -0500

    Newbox
    Co-Authored-By: Rangi <35663410+Rangi42@users.noreply.github.com>
    Co-Authored-By: Fredrik Ljungdahl <fredde1994@gmail.com>

--- engine/link/link.asm
+++ engine/link/link.asm
@@ -2278,7 +2278,7 @@ LinkTrade:
 
 	xor a ; REMOVE_PARTY
 	ld [wPokemonWithdrawDepositParameter], a
-	callfar RemoveMonFromPartyOrBox
+	callfar RemoveMonFromParty
 	ld a, [wPartyCount]
 	dec a
 	ld [wCurPartyMon], a
